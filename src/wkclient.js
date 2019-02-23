936c6e68a5aead15063ebb9025dd7765e62206f6
Yuri-M-Dias <yurimathe.yp@gmail.com>
2019-02-23 20:28:46 -0300

    Fixing the tests

--- src/wkclient.js
+++ src/wkclient.js
@@ -4,25 +4,20 @@
  * Base Client for sending requests to the WaniKani API
  */
 class Client {
-    /**
-     * Construct the client
-     */
-    constructor(options, baseURI) {
-        this.options = options || {};
-        this.baseURI = baseURI || process.env.WK_URI || 'https://api.wanikani.com/v2/';
-    }
+  /**
+   * Construct the client
+   */
+  constructor(options, baseURI) {
+    this.options = options || {};
+    this.baseURI =
+        baseURI || process.env.WK_URI || 'https://api.wanikani.com/v2/';
+  }
 
-    getOptions() {
-        return this.options;
-    }
+  getOptions() { return this.options; }
 
-    getWKURI() {
-        return this.WK_URI;
-    }
+  getWKURI() { return this.baseURI; }
 
-    add(x, y) {
-        return x + y;
-    }
+  add(x, y) { return x + y; }
 
 }
 
--- test/test.js
+++ test/test.js
@@ -5,32 +5,42 @@ const WK = require('../');
 var assert = require('assert');
 
 describe('Client', function() {
+
   describe('intialize', function() {
+
     it('empty should work', function() {
       const client = new WK.Client();
-      const resultingURI = client.getWKURI();
+      let resultingURI = client.getWKURI();
       assert.equal(resultingURI, 'https://api.wanikani.com/v2/');
       const resultingOptions = client.getOptions();
     });
+
     it('define wk URI', function() {
       const client = new WK.Client({}, 'test');
       const resultingURI = client.getWKURI();
       assert.equal(resultingURI, 'test');
     });
+
     it('define options', function() {
       const client = new WK.Client({test : 'test'});
       const resultingOptions = client.getOptions();
       assert.equal(resultingOptions.test, 'test');
     });
+
   });
+
   describe('add', function() {
+
     it('should add 1+1', function() {
       const client = new WK.Client();
-      assert.equal(client.add(1, 1), 4);
+      assert.equal(client.add(1, 1), 2);
     });
+
     it('should add 2+2', function() {
       const client = new WK.Client();
       assert.equal(client.add(2, 2), 4);
     });
+
   });
+
 });
